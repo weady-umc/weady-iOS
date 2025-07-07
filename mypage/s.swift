43d5369f9fb092278dacfa11a7c5fcf3f6bbeb10
keemeoow <112809954+keemeoow@users.noreply.github.com>
2025-07-07 20:11:12 +0900

    Create s.swift

---
+++ mypage/s.swift
@@ -0,0 +1,18 @@
+//
+//  s.swift
+//  
+//
+//  Created by 김지우 on 7/7/25.
+//
+
+import SwiftUI
+
+struct s: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    s()
+}
