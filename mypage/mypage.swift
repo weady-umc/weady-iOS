8cd16005d62bd693e1b7513fb454ca43bb91fcf6
keemeoow <112809954+keemeoow@users.noreply.github.com>
2025-07-07 20:05:26 +0900

    Update mypage.swift

--- mypage/mypage.swift
+++ mypage/mypage.swift
@@ -5,3 +5,14 @@
 //  Created by 김지우 on 7/7/25.
 //
 
+import SwiftUI
+
+struct mypage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mypage()
+}
