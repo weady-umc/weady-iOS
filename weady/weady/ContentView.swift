604c4c6fab8d5776f99b00a58d7fdaf4caeede7a
seo1v <minseoeum14@gmail.com>
2025-07-09 22:05:30 +0900

    [Chore] 프로젝트 초기 세팅

---
+++ weady/weady/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weady
+//
+//  Created by 엄민서 on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weady/weady/Sources/Feature/Weather/Views/Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  weady
+//
+//  Created by 엄민서 on 7/9/25.
+//
+
