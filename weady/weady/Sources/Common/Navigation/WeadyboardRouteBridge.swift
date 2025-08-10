d3ff43036a155b16d0f515dacd3c3e4b19448e7b
seo1v <minseoeum14@gmail.com>
2025-08-10 17:36:26 +0900

    [Feat] 네비게이션 관련 파일 추가

---
+++ weady/weady/Sources/Common/Navigation/WeadyboardRouteBridge.swift
@@ -0,0 +1,19 @@
+//
+//  WeadyboardRouteBridge.swift
+//  weady
+//
+//  Created by 엄민서 on 8/10/25.
+//
+
+import SwiftUI
+import Observation
+
+// MARK: - WeadyboardRouteBridge
+/// 다른 탭에서 보드 플로우로 연결
+@Observable
+final class WeadyboardRouteBridge: ObservableObject {
+    // MARK: Properties
+    var handler: ((WeadyboardRoute) -> Void)?
+    // MARK: Methods
+    func navigate(_ route: WeadyboardRoute) { handler?(route) }
+}
