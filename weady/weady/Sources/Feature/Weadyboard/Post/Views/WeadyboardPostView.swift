f7acb1841bdd0f91e47471946d31bd2fb638df0c
seo1v <minseoeum14@gmail.com>
2025-08-14 10:00:47 +0900

    [Feat/#71] 보드 게시물 스크랩 연결

--- weady/weady/Sources/Feature/Weadyboard/Post/Views/WeadyboardPostView.swift
+++ weady/weady/Sources/Feature/Weadyboard/Post/Views/WeadyboardPostView.swift
@@ -16,15 +16,15 @@ struct WeadyboardPostView: View {
     
     let boardId: Int
     @StateObject private var viewModel: WeadyboardPostViewModel
-    @StateObject private var reportViewModel: WeadyboardReportViewModel
-
+    @StateObject private var reportVM: WeadyboardReportViewModel
+    @EnvironmentObject private var weadychiveVM: WeadychiveViewModel
     @StateObject private var tagVM = TagViewModel()
 
     init(boardId: Int, isTabBarHidden: Binding<Bool>) {
         self.boardId = boardId
         self._isTabBarHidden = isTabBarHidden
         _viewModel = StateObject(wrappedValue: WeadyboardPostViewModel(boardId: boardId))
-        _reportViewModel = StateObject(wrappedValue: WeadyboardReportViewModel())
+        _reportVM = StateObject(wrappedValue: WeadyboardReportViewModel())
     }
 
     var body: some View {
@@ -55,6 +55,7 @@ struct WeadyboardPostView: View {
                             goodCount: viewModel.post?.goodCount ?? 0,
                             commentCount: 0,
 //                            commentCount: viewModel.post?.commentCount ?? 0,
+                            isScraped: weadychiveVM.isScrapped(boardId: boardId),
                             onLikeTap: {
                                 if viewModel.post?.goodStatus == true {
                                     viewModel.unlikeBoard()
@@ -63,7 +64,9 @@ struct WeadyboardPostView: View {
                                 }
                             },
                             onCommentTap: { showCommentSheet = true },
-                            onBookmarkTap: { }
+                            onBookmarkTap: {
+                                weadychiveVM.toggleBoardScrap(boardId: boardId)
+                            }
                         )
                         
                         WeadyboardContentView(
@@ -91,6 +94,7 @@ struct WeadyboardPostView: View {
             isTabBarHidden = true
             viewModel.fetchPostDetail()
             tagVM.loadAll(initialCriteria: .init())
+            weadychiveVM.fetchScrappedBoards()
         }
         .onDisappear {
             isTabBarHidden = false
@@ -104,7 +108,7 @@ struct WeadyboardPostView: View {
             WeadyboardPostMoreActionSheet(
                 showReportSheet: $showReportSheet,
                 boardId: boardId,
-                reportViewModel: reportViewModel
+                reportViewModel: reportVM
             )
             .presentationDetents([.height(255)])
             .presentationDragIndicator(.visible)
@@ -112,7 +116,7 @@ struct WeadyboardPostView: View {
         .sheet(isPresented: $showReportSheet) {
             WeadyboardPostReportNavigationSheet(
                 boardId: boardId,
-                reportViewModel: reportViewModel
+                reportViewModel: reportVM
             )
             .presentationDetents([.height(759)])
             .presentationDragIndicator(.visible)
