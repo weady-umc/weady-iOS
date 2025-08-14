9897ba46dcd8dd5002b098a6071839cd5d42b9c1
yys-63 <yangys0630@naver.com>
2025-08-15 01:09:05 +0900

    [Fix]

--- weady/weady/Sources/Feature/Home/Views/HomeView.swift
+++ weady/weady/Sources/Feature/Home/Views/HomeView.swift
@@ -112,7 +112,7 @@ struct HomeView: View {
         .onAppear {
             // 토큰 세팅
             UserDefaults.standard.set(
-                "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNCIsImVtYWlsIjoieWFuZ3lzMDYzMEBuYXZlci5jb20iLCJwcm92aWRlciI6IktBS0FPIiwiZXhwIjoxNzU1MTgzMDczfQ.FA0WXJieO-2cQsi-I8ig-7PSMfubAmn0gUUfZmjo_CQaspP9bvhhAUTEEzrxHvTGTL7mMf5ZJWYKwSxaDlxgUQ",
+                "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNCIsImVtYWlsIjoieWFuZ3lzMDYzMEBuYXZlci5jb20iLCJwcm92aWRlciI6IktBS0FPIiwiZXhwIjoxNzU1MTg5NTM1fQ.KV1y4_XRyx2BhI3C0zzvZAYSyYcdy_XzxT3VVkcUjE-ql-6wWNzYtWqNtjSRjQ9QKs-FImkyt2G6WX483El9Qw",
                 forKey: "accessToken"
                 
                 
--- weady/weady/Sources/Feature/Weather/Views/WeatherHomeView.swift
+++ weady/weady/Sources/Feature/Weather/Views/WeatherHomeView.swift
@@ -50,7 +50,7 @@ struct WeatherHomeView: View {
                 }
             }
             .onAppear {
-                UserDefaults.standard.set("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNCIsImVtYWlsIjoieWFuZ3lzMDYzMEBuYXZlci5jb20iLCJwcm92aWRlciI6IktBS0FPIiwiZXhwIjoxNzU1MTgzMDczfQ.FA0WXJieO-2cQsi-I8ig-7PSMfubAmn0gUUfZmjo_CQaspP9bvhhAUTEEzrxHvTGTL7mMf5ZJWYKwSxaDlxgUQ", forKey: "accessToken")
+                UserDefaults.standard.set("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNCIsImVtYWlsIjoieWFuZ3lzMDYzMEBuYXZlci5jb20iLCJwcm92aWRlciI6IktBS0FPIiwiZXhwIjoxNzU1MTg5NTM1fQ.KV1y4_XRyx2BhI3C0zzvZAYSyYcdy_XzxT3VVkcUjE-ql-6wWNzYtWqNtjSRjQ9QKs-FImkyt2G6WX483El9Qw", forKey: "accessToken")
             }
             
         }
