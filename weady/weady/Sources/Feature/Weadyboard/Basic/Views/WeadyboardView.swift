c854f6123ee8b7b7d57acdca54b30621b044a96e
seo1v <minseoeum14@gmail.com>
2025-08-17 05:23:24 +0900

    [Design/#118] 네비바 여백 수정

--- weady/weady/Sources/Feature/Weadyboard/Basic/Views/WeadyboardView.swift
+++ weady/weady/Sources/Feature/Weadyboard/Basic/Views/WeadyboardView.swift
@@ -29,7 +29,7 @@ struct WeadyboardView: View {
             VStack(spacing: 0) {
                 CustomNavBar(
                     viewTitle: "",
-                    showBackButton: false,
+                    showLogoButton: true,
                     showAlarmButton: true,
                     showBottomDivider: false
                 )
