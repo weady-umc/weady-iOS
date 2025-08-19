85e51eeb36668209845a04d6073f48dadf188257
gogo <daehcjf@gmail.com>
2025-08-20 01:58:05 +0900

    베이스탭 수정

--- weady/weady/Service/Curation/Endpoints/CurationEndpoints.swift
+++ weady/weady/Service/Curation/Endpoints/CurationEndpoints.swift
@@ -64,24 +64,24 @@ extension CurationEndpoints: TargetType {
         return .requestPlain
     }
 
-//    var headers: [String: String]? {
-//        let keychain = KeychainSwift()
-//        if let token = keychain.get("serverAccessToken") {
-//            return [
-//                "Authorization": "Bearer \(token)",
-//                "Content-Type": "application/json"
-//            ]
-//        } else {
-//            return [
-//                "Content-Type": "application/json"
-//            ]
-//        }
-//    }
     var headers: [String: String]? {
-        return [
-            "Content-Type": "application/json",
-            "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNiIsImVtYWlsIjoiZGFlaGNqZkBnbWFpbC5jb20iLCJwcm92aWRlciI6IkdPT0dMRSIsImV4cCI6MTc1NTYyNTQyOH0.hl8a4nyq9IfYnwDKEKGJnOHOIu5WYwRp5znIQDwvl4oCsmFjdVwSfErVXBXXnVYyGxto8UzK5k_rFsfAS3nwDA"
-        ]
+        let keychain = KeychainSwift()
+        if let token = keychain.get("serverAccessToken") {
+            return [
+                "Authorization": "Bearer \(token)",
+                "Content-Type": "application/json"
+            ]
+        } else {
+            return [
+                "Content-Type": "application/json"
+            ]
+        }
     }
+//    var headers: [String: String]? {
+//        return [
+//            "Content-Type": "application/json",
+//            "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyNiIsImVtYWlsIjoiZGFlaGNqZkBnbWFpbC5jb20iLCJwcm92aWRlciI6IkdPT0dMRSIsImV4cCI6MTc1NTYyNTQyOH0.hl8a4nyq9IfYnwDKEKGJnOHOIu5WYwRp5znIQDwvl4oCsmFjdVwSfErVXBXXnVYyGxto8UzK5k_rFsfAS3nwDA"
+//        ]
+//    }
     
 }
